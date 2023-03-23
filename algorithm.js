b993e0519ef189cc65e3e163bcff51af61d9137b
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-03-23 00:51:05 -0500

    Path finding working doing the visuals now

--- algorithm.js
+++ algorithm.js
@@ -2,46 +2,51 @@ const node_graph = []
 
 const knight_moves = [[-1, 2], [1, 2], [-2, 1], [2, 1], [-1, -2], [1, -2], [-2, -1], [2, -1]]
 const king_moves = [[1, 1], [0, 1], [-1, 1], [-1, 0], [1, 0], [1, -1], [0, -1], [-1, -1]]
-const moves = king_moves
+const moves = knight_moves
 
-function getPos(index)
-{
-    return [ index%8, Math.floor(index/8) ]
+function getPos(index) {
+    return [index % 8, Math.floor(index / 8)]
 }
 
-function getIndex(pos)
-{
+function getIndex(pos) {
     return pos[0] + pos[1] * 8
 }
 
-for (let y = 0; y < 8; y++)
-{
-    for (let x = 0; x < 8; x++)
-    {
+function withinBounds(x, y) {
+    return x > -1 && x < 8 && y > -1 && y < 8
+}
+
+for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
         const node = {
             visited: false,
-            points: []
+            index: getIndex([x, y]),
+            points: [],
+            x: x,
+            y: y
         }
 
         moves.map((move, index) => {
-            if (x+move[0] > -1 && x+move[0] < 8 && y+move[1] > -1 && y+move[1] < 8)
-                node.points.push( x+move[0], y+move[1] )
+            if (withinBounds(x + move[0], y + move[1]))
+                node.points.push([x + move[0], y + move[1]])
         })
 
         node_graph.push(node)
     }
 }
 
-function score(node1, node2)
-{
-    const pos1 = getPos(node1)
-    const pos2 = getPos(node2)
+function score(node1, node2) {
+    const pos1 = getPos(node1.index)
+    const pos2 = getPos(node2.index)
 
     return Math.hypot(pos1[0] - pos2[0], pos1[1] - pos2[1])
 }
 
-function Path(start, finish)
-{
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function Path(start, finish) {
     const open_list = [start]
     const closed_list = []
 
@@ -51,35 +56,96 @@ function Path(start, finish)
     start.g = 0
     start.f = 0
 
-    function getG(node)
-    {
+    function getG(node) {
         return node.parent.g + score(node, node.parent)
     }
 
-    function getH(node)
-    {
+    function getH(node) {
         return score(node, finish)
     }
 
-    while (open_list.length > 0)
-    {
+    function calculatePath(node) {
+        const path = []
+        while (node.parent != node) {
+            path.push(node)
+            node = node.parent
+
+        }
+
+        return path.reverse()
+    }
+
+    while (open_list.length > 0) {
         // Finding Node with lowest f
         let q = open_list[0]
         let qi = 0
         let smallestF = getG(open_list[0]) + getH(open_list[0])
 
-        for (let i = 1; i < open_list.length; i++)
-        {
+        for (let i = 1; i < open_list.length; i++) {
             let f = getG(open_list[i]) + getH(open_list[i])
 
-            if (smallestF > f)
-            {
+            if (smallestF > f) {
                 smallestF = f
                 q = open_list[i]
                 qi = i
             }
         }
 
+        // closed_list.push(q)
         open_list.splice(qi, 1)
+
+        // Collecting Successors
+        const successors = []
+        q.points.map(point => {
+            const newNode = { ...node_graph[getIndex(point)] }
+            newNode.parent = q
+            successors.push(newNode)
+        })
+
+        for (let i = 0; i < successors.length; i++) {
+            const successor = successors[i]
+            if (successor.index == finish.index)
+                return calculatePath(successor)
+
+            successor.g = getG(successor)
+            successor.h = getH(successor)
+            successor.f = successor.g + successor.h
+
+            // document.getElementById("tile"+40).style.backgroundColor = "red"
+
+            // document.getElementById("tile"+successor.index).style.backgroundColor = "red"
+            // document.getElementById("tile"+successor.index).style.color = "red"
+            // document.getElementById("tile"+successor.index).innerHTML = Math.round(successor.f*100)/100
+
+            let skip = false
+            for (let j = 0; j < open_list.length; j++) {
+                if (open_list[j].index == successor.index && successor.f > open_list[j].f) {
+                    skip = true
+                    break
+                }
+            }
+
+            if (skip)
+                continue
+
+            for (let j = 0; j < closed_list.length; j++) {
+                if (closed_list[j].index == successor.index && successor.f > closed_list[j].f) {
+                    skip = true
+                    break
+                }
+            }
+
+            if (!skip)  
+                open_list.push(successor)
+
+            // await sleep(100)
+        }
+
+        closed_list.push(q)
+
+
     }
+
+    console.log("rip")
+    return []
 }
--- index.js
+++ index.js
@@ -2,17 +2,51 @@ const checkerboard = document.getElementById("board");
 const tilecolors = ["tan", "black"];
 const tiles = [];
 
+function isWhite(index)
+{
+  return ((Math.floor(i / 8) % 2) + i) % 2 == 0
+}
+
+let pause = false
+let targetPath = []
+
 for (var i = 0; i < 64; i++) {
   const div = document.createElement("div", { class: "" });
-  // div.id="tile"+i
-  div.style.background = tilecolors[((Math.floor(i / 8) % 2) + i) % 2];
+  div.classList.add(isWhite(i)?"white":"black")
+  div.id="tile"+i
   checkerboard.appendChild(div);
+
+  div.onclick = () => {
+    pause = true
+    Path(node_graph)
+  }
+
   tiles[i] = div;
 }
+
+const boardScale = .9
+const boardSize = Math.min(
+  document.body.getBoundingClientRect().width*boardScale, 
+  document.body.getBoundingClientRect().height*boardScale)
+
+checkerboard.style.width = boardSize+"px"
 
 const knight = document.createElement("img");
 knight.classList.add("knight");
 knight.src = "Knight.png";
-knight.style.height = checkerboard.height / 8;
-knight.style.width = checkerboard.height / 8;
+knight.style.height = (boardSize / 8)+"px";
+knight.style.width = (boardSize / 8)+"px";
 document.body.appendChild(knight);
+
+let knightPos = 32
+
+setInterval(() => {
+
+}, .5)
+
+Path(node_graph[0], node_graph[11]).then((thing) => {
+  thing.map((node) => {
+    const div = document.getElementById("tile"+node.index)
+    div.style.backgroundColor = "red"
+  })
+})
